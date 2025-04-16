7c850a3135a500e1d029865d196ba25fd89c7412
daslu <daniel.slutsky@gmail.com>
2025-04-16 22:30:33 +0300

    website setup

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,5 +1,3 @@
-;; # Preface
-
 ^{:clay {:hide-code true}}
 (ns index
   (:require [scicloj.kindly.v4.kind :as kind]))
@@ -10,15 +8,16 @@
 
 ;; It is a first in a series of conferences that will explore new fields and use case for Clojure, and specifically the [Noj](https://scicloj.github.io/noj/) data science toolkit.
 
-;; <a class="btn btn-primary btn-sm px-4 mb-2" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
+;; <a class="btn btn-secondary btn-sm px-4 mb-2" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
 
-;; ## Timeline
-;; * May 10 - [prep workshop](https://scicloj.github.io/blog/data-analyis-with-clojure-free-workshop-may-10th-initial-survey/)
+;; # Timeline
+;; * May 10 - [Prep workshop](https://scicloj.github.io/blog/data-analyis-with-clojure-free-workshop-may-10th-initial-survey/)
 ;; * May 16-17 - [Conference](https://clojureverse.org/t/scinoj-light-1st-conference/)
 
-;; ## Chat
+;; # Chat
 ;; All communication in preparation and during the conference will take place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
 
-;; ## Participating
-;; Participating is online and free.
+;; # Participating
+;; Participating is online and free but requires registration.
+;; We will soon share the registration form.
 
--- notebooks/render.clj
+++ notebooks/render.clj
@@ -1,10 +1,36 @@
 (ns render
-  (:require [scicloj.clay.v2.api :as clay]))
+  (:require [scicloj.clay.v2.api :as clay]
+            [clj-yaml.core :as yaml]))
+
+#_(clay/make! {:format [:quarto :html]
+               :base-source-path "notebooks"
+               :source-path ["index.clj"
+                             "speakers.clj"]
+               :base-target-path "docs"
+               :book {:title "SciNoj Light 1"}
+               :clean-up-target-dir true})
 
 (clay/make! {:format [:quarto :html]
              :base-source-path "notebooks"
              :source-path ["index.clj"
-                           "speakers.clj"]
+                           "speakers.clj"
+                           "hosts.clj"]
+             :run-quarto false
              :base-target-path "docs"
-             :book {:title "SciNoj Light 1"}
-             :clean-up-target-dir true})
+             ;; :clean-up-target-dir true
+             })
+
+(->> {:project {:type :website}
+      :website {:title "SciNoj Light #1"
+                :navbar {:search true
+                         :left [{:href "index.qmd"
+                                 :text "Home"}
+                                "speakers.qmd"
+                                "hosts.qmd"]}}
+      :format {:html {:theme "flatly"
+                      :linkcolor "#440044"
+                      ;; :css "styles.css"
+                      :toc true}}}
+     yaml/generate-string
+     (spit "docs/_quarto.yml"))
+
