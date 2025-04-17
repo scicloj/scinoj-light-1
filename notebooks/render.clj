720f837a7f3ed2a1c549f7e57bf01e19fd5c9ae6
daslu <daniel.slutsky@gmail.com>
2025-04-17 15:27:20 +0300

    updated book workflow

--- notebooks/render.clj
+++ notebooks/render.clj
@@ -1,6 +1,11 @@
 (ns render
   (:require [scicloj.clay.v2.api :as clay]
-            [clj-yaml.core :as yaml]))
+            [scicloj.clay.v2.server]
+            [clj-yaml.core :as yaml]
+            [clojure.java.shell :as shell]
+            [babashka.fs :as fs]))
+
+(scicloj.clay.v2.server/loading!)
 
 (clay/make! {:format [:quarto :html]
              :base-source-path "notebooks"
@@ -10,10 +15,10 @@
                            "sessions.clj"
                            "schedule.clj"]
              :run-quarto false
+             :show false
              :base-target-path "website"})
 
-(->> {:project {:type :website
-                :output-dir :docs}
+(->> {:project {:type :website}
       :website {:title "SciNoj Light #1"
                 :navbar {:search true
                          :left [{:href "index.qmd"
@@ -28,4 +33,23 @@
                       :toc true}}}
      yaml/generate-string
      (spit "website/_quarto.yml"))
+
+(->> (shell/with-sh-dir
+       "website"
+       (shell/sh "quarto" "render"))
+     ((juxt :err :out))
+     (mapv println))
+
+(babashka.fs/delete-tree "docs")
+
+(babashka.fs/copy-tree "website/_site"
+                       "docs"
+                       {:replace-existing true})
+
+(babashka.fs/delete-tree "website/_site")
+
+(scicloj.clay.v2.server/update-page!
+ {:show true
+  :base-target-path "docs"
+  :full-target-path "docs/index.html"})
 
