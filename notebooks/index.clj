0edb8c01e7a56f20dec49e0d56eea94be32391c5
daslu <daniel.slutsky@gmail.com>
2025-04-19 02:38:18 +0300

    index page

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -8,18 +8,17 @@
 
 ;; It is a first in a series of conferences that will explore new fields and use case for Clojure, and specifically the [Noj](https://scicloj.github.io/noj/) data science toolkit.
 
-;; <a class="btn btn-info btn-lg px-4 mb-2 rounded" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
-
 ;; # Timeline
 ;; * May 10 - [Prep workshop](https://scicloj.github.io/blog/data-analyis-with-clojure-free-workshop-may-10th-initial-survey/)
 ;; * May 16-17 - Conference: [Event](https://clojureverse.org/t/scinoj-light-1st-conference/), [Tentative schedule](./schedule.html)
-
-;; # Chat
-;; All communication in preparation and during the conference will take place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
 
 ;; # Participating
 ;; Participating is online and free, but we may have to limit the number of participants.
 
 ;; <a class="btn btn-success btn-lg px-4 mb-2 rounded" href="https://forms.gle/AswsDYaYTFckNJ2j7" role="button">Please register till May 12th</a>
 
+;; # Chat
+;; All communication in preparation and during the conference will take place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
 
+
+;; <a class="btn btn-info btn-lg px-4 mb-2 rounded" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
