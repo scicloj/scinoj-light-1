f7e4df93f79e6297fcdfc8c9467a6edabbe8e788
daslu <daniel.slutsky@gmail.com>
2025-04-18 20:40:53 +0300

    styling

--- notebooks/schedule.clj
+++ notebooks/schedule.clj
@@ -47,13 +47,15 @@
                                 (kind/hiccup
                                  [:div
                                   (if abstract
-                                    [:div {:style {:background-color
+                                    [:div {:style {:border-radius "10px"
+                                                   :background-color
                                                    (case session-type
                                                      :special "#ffeeff"
                                                      :background "#ffffee"
                                                      :data-analysis "#eeffff"
                                                      "#ffffff")}} 
-                                     [:details
+                                     [:details {:style {:margin "20px"
+                                                        }}
                                       [:summary title]
                                       [:div
                                        [:i (some->> session-type
