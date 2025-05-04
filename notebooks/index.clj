88d7e27667acf36ae4fdde339f05654770483d62
daslu <daniel.slutsky@gmail.com>
2025-05-04 23:34:13 +0300

    index page

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -20,5 +20,6 @@
 ;; # Chat
 ;; All communication in preparation and during the conference will take place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
 
+;; Please join the chat before the conference, and learn a bit about [channels](https://zulip.com/help/introduction-to-channels) and [topics](https://zulip.com/help/introduction-to-topics). You are encouraged to introduce yourself at the [**#scinoj-light-1 > hello**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1/topic/hello/) topic thread.
 
 ;; <a class="btn btn-info btn-lg px-4 mb-2 rounded" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
