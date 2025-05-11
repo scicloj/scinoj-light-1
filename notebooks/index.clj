dfe7c052c43bc2d2b0db80e48761677ee7cccff9
daslu <daniel.slutsky@gmail.com>
2025-05-11 14:54:11 +0300

    index page

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -9,7 +9,7 @@
 ;; It is a first in a series of conferences that will explore new fields and use case for Clojure, and specifically the [Noj](https://scicloj.github.io/noj/) data science toolkit.
 
 ;; ## Timeline
-;; * May 10 - ✏ Preparation workshop ⭐[new page](/workshop.html)⭐
+;; * May 10 - ✏ [Preparation workshop](/workshop.html): data analysis in Clojure
 ;; * May 16-17 - Conference: 📅 [Event](https://clojureverse.org/t/scinoj-light-1st-conference/), 🛠 [Tentative schedule](./schedule.html)
 
 ;; ## Participating
