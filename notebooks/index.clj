766881632e0338e7c3f8243468cf0fd94739b9c0
daslu <daniel.slutsky@gmail.com>
2025-05-18 23:47:22 +0300

    index page

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -4,9 +4,9 @@
 
 ;; ![](notebooks/images/Noj.png){fig-alt="Noj logo"}
 
-;; SciNoj Light is an online conference where Clojurians will share their short-term data analysis stories.
+;; SciNoj Light #1 was an online conference where Clojurians shared their short-term data analysis stories.
 
-;; It is a first in a series of conferences that will explore new fields and use case for Clojure, and specifically the [Noj](https://scicloj.github.io/noj/) data science toolkit.
+;; It was a first in a series of conferences that will explore new fields and use case for Clojure, and specifically the [Noj](https://scicloj.github.io/noj/) data science toolkit.
 
 ;; ## Timeline
 ;; * May 10 - we had a [preparation workshop](/workshop.html): data analysis in Clojure
@@ -14,9 +14,13 @@
 ;;   * 📅 [Event](https://clojureverse.org/t/scinoj-light-1st-conference/)
 ;;   * ⏰ [Schedule](./schedule.html)
 
-;; ## Chat
-;; All communication in preparation and during the conference will take place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
+;; ## Recordings
 
-;; Please join the chat before the conference, and learn a bit about [channels](https://zulip.com/help/introduction-to-channels) and [topics](https://zulip.com/help/introduction-to-topics). You are encouraged to introduce yourself at the [**#scinoj-light-1 > hello**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1/topic/hello/) topic thread.
+;; The recordings were shared (as unlisted videos) internally at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/) under the [#**scinoj-light-1>recordings**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1/topic/recordings/) topic thread.
+
+;; Some parts will be shared publicly in the near future.
+
+;; ## Chat
+;; All communication of the conference took place at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/), specifically in the [#**scinoj-light-1**](https://clojurians.zulipchat.com/#narrow/channel/479601-scinoj-light-1) channel.
 
 ;; <a class="btn btn-info btn-lg px-4 mb-2 rounded" href="https://scicloj.github.io/docs/community/contact/" role="button">Any questions? Let us talk</a>
